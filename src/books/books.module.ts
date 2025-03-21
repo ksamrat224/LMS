b1abb968751054f3ec729319aa843c928fcfc11e
ksamrat224 <ksamrat224@gmail.com>
2025-03-21 14:08:07 +0545

    book dto completed

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService, PrismaClient],
 })
 export class BooksModule {}
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,26 @@
-export class CreateBookDto {}
+import {
+  IsBoolean,
+  IsEmpty,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  author: string;
+
+  @IsOptional()
+  @IsNumber()
+  quantity: number;
+
+  @IsBoolean()
+  @IsEmpty()
+  availability: boolean;
+}
