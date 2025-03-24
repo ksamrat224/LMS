056734f5805ba2b959699fbf7c7156027ab467f4
ksamrat224 <ksamrat224@gmail.com>
2025-03-24 20:20:24 +0545

    member dto updated

--- src/members/dto/create-member.dto.ts
+++ src/members/dto/create-member.dto.ts
@@ -1,6 +1,17 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateMemberDto {
+  @IsOptional()
+  @IsNumber()
+  user_id: number;
+
   @IsNotEmpty()
   @IsString()
   name: string;
