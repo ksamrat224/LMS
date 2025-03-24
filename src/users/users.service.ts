470c18d23cc138588f5d683c05c563b5393ee02a
ksamrat224 <ksamrat224@gmail.com>
2025-03-24 14:55:00 +0545

    password hashing ,email and mobile uniqueness done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -10,7 +15,24 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto) {
     //logic to manage uniqueness of data i.e email & mobile
+    let user = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+    if (user) {
+      throw new BadRequestException('This email has already been taken');
+    }
+    user = await this.prisma.user.findUnique({
+      where: { mobile: createUserDto.mobile },
+    });
+    if (user) {
+      throw new BadRequestException(
+        'This mobile number has already been taken',
+      );
+    }
+    //hash password
+    createUserDto.password = await hash(createUserDto.password, 10);
 
+    //create user
     return this.prisma.user.create({
       data: createUserDto,
     });
@@ -31,7 +53,32 @@ export class UsersService {
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
+    let user: User | null;
     await this.findOne(id);
+
+    if (updateUserDto.email) {
+      user = await this.prisma.user.findUnique({
+        where: { email: updateUserDto.email },
+      });
+      if (user && user.id != id) {
+        throw new BadRequestException('This email is already registered');
+      }
+    }
+    if (updateUserDto.mobile) {
+      user = await this.prisma.user.findUnique({
+        where: { mobile: updateUserDto.mobile },
+      });
+      if (user && user.id != id) {
+        throw new BadRequestException(
+          'This mobile no. has already been registered',
+        );
+      }
+    }
+    //hash password here
+    user = await this.findOne(id);
+    if (updateUserDto.password) {
+      updateUserDto.password = await hash(updateUserDto.password, 10);
+    }
     return this.prisma.user.update({
       where: { id },
       data: updateUserDto,
