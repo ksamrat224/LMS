9e5de805027766786e1e3f1db3f73da0c922b028
ksamrat224 <ksamrat224@gmail.com>
2025-03-24 20:31:33 +0545

    unique data in member.service

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
-import { PrismaClient } from '@prisma/client';
+import { Member, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class MembersService {
@@ -9,6 +13,20 @@ export class MembersService {
   constructor(private readonly prisma: PrismaClient) {}
 
   async create(createMemberDto: CreateMemberDto) {
+    //Logic to manage uniqueness of data i.e email & mobile
+    let member = await this.prisma.member.findUnique({
+      where: { email: createMemberDto.email },
+    });
+    if (member) {
+      throw new BadRequestException('This email has already been taken');
+    }
+    member = await this.prisma.member.findUnique({
+      where: { mobile: createMemberDto.mobile },
+    });
+    if (member) {
+      throw new BadRequestException('This mobile number is already taken');
+    }
+    //create member here
     return this.prisma.member.create({
       data: createMemberDto,
     });
@@ -29,7 +47,26 @@ export class MembersService {
   }
 
   async update(id: number, updateMemberDto: UpdateMemberDto) {
+    let member: Member | null;
     await this.findOne(id);
+    if (updateMemberDto.email) {
+      member = await this.prisma.member.findUnique({
+        where: { email: updateMemberDto.email },
+      });
+      if (member && member.id != id) {
+        throw new BadRequestException('This email has already been registered');
+      }
+    }
+    if (updateMemberDto.mobile) {
+      member = await this.prisma.member.findUnique({
+        where: { mobile: updateMemberDto.mobile },
+      });
+      if (member && member.id != id) {
+        throw new BadRequestException(
+          'This mobile has already been registered',
+        );
+      }
+    }
     return this.prisma.member.update({
       where: { id },
       data: updateMemberDto,
