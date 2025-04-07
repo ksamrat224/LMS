b34291ebdb5361caf7b5b554d737f1dc93a9e51f
ksamrat224 <ksamrat224@gmail.com>
2025-04-07 13:57:29 +0545

    transaction dto updated

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,20 @@
-export class CreateTransactionDto {}
+import { ReservationType } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsOptional()
+  @IsNumber()
+  user_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  book_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  member_id: number;
+
+  @IsOptional()
+  @IsEnum(ReservationType)
+  type: ReservationType;
+}
