97a26cc770efab76aac400089d419fb94b1d4e3d
ksamrat224 <ksamrat224@gmail.com>
2025-04-13 14:03:16 +0545

    cloudinary setup completed

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  async uploadFile(image: string): Promise<string> {
+    try {
+      const result = await cloudinary.uploader.upload(image);
+      return result?.secure_url;
+    } catch (error) {
+      throw new Error(`Cloudinary upload failed:${error.message}`);
+    }
+  }
+}
