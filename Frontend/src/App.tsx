98a4bc9c4714564bba842e1981f1ee180991641f
ksamrat224 <ksamrat224@gmail.com>
2025-04-21 14:57:11 +0545

    login page created and routing performed

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router"
 import Register from "./pages/Register"
+import Login from "./pages/Login"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>} />
       
     </Routes>
   )
