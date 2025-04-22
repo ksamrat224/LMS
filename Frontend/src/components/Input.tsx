7085d5c5bc6fd2067937e27c7f9b4f4a1ed569f7
ksamrat224 <ksamrat224@gmail.com>
2025-04-22 13:52:23 +0545

    eye overlap css fixed

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -31,18 +31,18 @@ const Input = ({ content, type, id, name }: InputProps) => {
           id={id}
           name={name}
           required
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full px-4 py-2 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-       {isPassword && (
-  <button
-    type="button"
-    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600"
-    onClick={handleShowPassword}
-    aria-label={showPassword ? "Hide password" : "Show password"}
-  >
-    <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
-  </button>
-)}
+        {isPassword && (
+          <button
+            type="button"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600"
+            onClick={handleShowPassword}
+            aria-label={showPassword ? "Hide password" : "Show password"}
+          >
+            <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+          </button>
+        )}
       </div>
     </div>
   );
