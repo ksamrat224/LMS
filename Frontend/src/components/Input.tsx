e68c991d736c27454ea573afc858e709f3340a2d
ksamrat224 <ksamrat224@gmail.com>
2025-04-22 14:02:29 +0545

    input page with improved css

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -24,22 +24,19 @@ const Input = ({ content, type, id, name }: InputProps) => {
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
         {content}
       </label>
-      <div className="relative">
+      <div className="flex items-center justify-between w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
         <input
           type={inputType}
           id={id}
           name={name}
           required
-          className="w-full px-4 py-2 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className={`border-none outline-0 ${
+            isPassword ? "w-[90%]" : "w-full"
+          }`}
         />
         {isPassword && (
-          <button
-            type="button"
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600"
-            onClick={handleShowPassword}
-            aria-label={showPassword ? "Hide password" : "Show password"}
-          >
-            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+          <button type="button" className="" onClick={handleShowPassword}>
+            {showPassword ? <EyeOff /> : <Eye />}
           </button>
         )}
       </div>
