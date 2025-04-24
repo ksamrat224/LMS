0542b71868e88b7fd20bace2a6c25daaa3c12316
ksamrat224 <ksamrat224@gmail.com>
2025-04-24 14:24:54 +0545

    protected route on register

--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -3,8 +3,10 @@ import Button from "../components/Button";
 import Input from "../components/Input";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import { useNavigate } from "react-router";
 
 const Register = () => {
+  const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState(""); // Define errorMessage state
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -22,6 +24,7 @@ const Register = () => {
       });
       localStorage.setItem("token", response.data.token);
          // Store the token in local storage
+      navigate("/"); // Redirect to the home page after successful registration
       console.log(response.data);
     } catch (error: any) {
       // Set the error message in case of an error
