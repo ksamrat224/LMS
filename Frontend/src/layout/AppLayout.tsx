f55db72dbb659889072bd64b9698d7fac119330d
ksamrat224 <ksamrat224@gmail.com>
2025-04-27 14:48:41 +0545

    applayout ui changed

--- Frontend/src/layout/AppLayout.tsx
+++ Frontend/src/layout/AppLayout.tsx
@@ -6,9 +6,7 @@ const AppLayout = () => {
     <div className="w-screen h-screen flex">
       <SideBar />
       <div className="w-[75%] h-full bg-gray-100 flex flex-col">
-        <header className="py-4 text-center bg-gradient-to-r from-purple-700 to-blue-700 shadow-lg">
-          <p className="text-gray-100 text-xl font-medium">Welcome to the LMS App</p>
-        </header>
+    
         <div className="flex-grow overflow-auto">
           <Outlet />
           {/* This is where the nested routes will be rendered */}
