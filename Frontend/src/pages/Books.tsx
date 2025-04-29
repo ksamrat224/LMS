869b40dae11162090432988b374b0865475be70e
ksamrat224 <ksamrat224@gmail.com>
2025-04-29 14:19:34 +0545

    on click navigation on pencil icon

--- Frontend/src/pages/Books.tsx
+++ Frontend/src/pages/Books.tsx
@@ -117,7 +117,8 @@ const Books = () => {
                   </td>
                   <td className="px-6 py-4 text-gray-900 border border-gray-300">
                     <div className="flex space-x-4">
-                      <PencilIcon className="text-blue-600 cursor-pointer" />
+                      <PencilIcon className="text-blue-600 cursor-pointer" 
+                       onClick={()=>navigate(`/edit-book/${book.id}`)} />
                       <TrashIcon className="text-red-600 cursor-pointer" />
                     </div>
                   </td>
