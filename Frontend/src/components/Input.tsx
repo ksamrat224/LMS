164db17cfec908f46d5fa05486fd42ccd7be6936
ksamrat224 <ksamrat224@gmail.com>
2025-04-30 11:19:44 +0545

    input field interface changed and destructured

--- Frontend/src/components/Input.tsx
+++ Frontend/src/components/Input.tsx
@@ -1,16 +1,16 @@
 import { Eye, EyeOff } from "lucide-react";
-import { MouseEvent, useState } from "react";
+import { InputHTMLAttributes, MouseEvent, useState } from "react";
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   type: string;
   id: string;
   name: string;
-  required?: boolean|false;
-  value?: string|number;
+  required?: boolean ;
+  value?: string | number;
 }
 
-const Input = ({ label, type, id, name ,required,value }: InputProps) => {
+const Input = ({ label, type, id, name, required, value,...rest }: InputProps) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleShowPassword = (e: MouseEvent<HTMLButtonElement>) => {
@@ -36,6 +36,7 @@ const Input = ({ label, type, id, name ,required,value }: InputProps) => {
           className={`border-none outline-0 ${
             isPassword ? "w-[90%]" : "w-full"
           }`}
+          {...rest}
         />
         {isPassword && (
           <button type="button" className="" onClick={handleShowPassword}>
