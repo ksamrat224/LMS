8ae938c671d4ce6af491f9da2f712f9c09add6a2
ksamrat224 <ksamrat224@gmail.com>
2025-05-01 20:50:41 +0545

    modal code updation

--- Frontend/src/components/Modal.tsx
+++ Frontend/src/components/Modal.tsx
@@ -1,10 +1,13 @@
 import Button from "./Button";
+interface ModalProps {
+  onConfirm: () => void;
+  onClose: () => void;
+}
 
-export default function CustomModal() {
+export default function Modal({isModelOpen, onConfirm, onClose}: ModalProps) {
   return (
-    <div className="w-screen h-screen bg-black/50 flex justify-center items-center fixed inset-0 z-50">
+    <div className={`w-screen h-screen bg-black/50 flex justify-center items-center fixed inset-0 z-50 ${isModelOpen ? 'block' : 'hidden'}`}>
       <div className="w-[500px] bg-white p-6 flex flex-col justify-between rounded-lg shadow-lg">
-        {/* Modal Header */}
         <div className="text w-full">
           <h1 className="text-xl font-bold text-gray-800">Delete Book</h1>
           <p className="text-gray-600 mt-2">
@@ -20,12 +23,14 @@ export default function CustomModal() {
               bgColor="px-4 py-2 bg-green-500 text-gray-800 rounded hover:bg-green-600"
               type="button"
               label="Cancel"
+              onClick={onClose}
             />
           </div>
           <Button
             bgColor="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
             type="button"
             label="Delete"
+            onClick={onConfirm}
           />
         </div>
       </div>
