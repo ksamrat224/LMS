82e7f9ce784f0b524b0051b26158556118e9b0a9
ksamrat224 <ksamrat224@gmail.com>
2025-05-04 14:30:37 +0545

    useMemo for managing rendering and type Theme for specifying only two bg

--- Frontend/src/context/themeContext.tsx
+++ Frontend/src/context/themeContext.tsx
@@ -1,25 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useMemo, useState } from "react";
 //contextAPI-for global data management
 //ThemeContext-for implementing dark/light mode toggle across the app
 //Context Provider-stores and manipulates context data
 //Context Consumer-uses context data in components
-interface ThemeContextValues{
-  theme: string;
-  setTheme:(value:string)=>void;
 
+type Theme = "light" | "dark"; // Define the possible themes
+interface ThemeContextValues {
+  theme: Theme;
+  setTheme: (value: Theme) => void;
 }
 const ThemeContext = createContext<ThemeContextValues>({
   theme: "light",
   setTheme: () => {}, // Default function to avoid errors
 });
 
-const ThemeProvider = ({children}:{children:React.ReactElement}) =>{
-    const[theme, setTheme] = useState("light");
-    return(
-        <ThemeContext.Provider value={{ theme,setTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
-
+const ThemeProvider = ({ children }: { children: React.ReactElement }) => {
+  const [theme, setTheme] = useState<Theme>("light");
+  const value = useMemo(() => ({ theme, setTheme }), [theme]);
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
 };
 export { ThemeContext, ThemeProvider };
