45eb5863c934048414610142b3dedee3a7c6c6ff
ksamrat224 <ksamrat224@gmail.com>
2025-05-04 13:58:53 +0545

    themeprovider created and wrapped

--- Frontend/src/context/themeContext.tsx
+++ Frontend/src/context/themeContext.tsx
@@ -1,6 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 //contextAPI-for global data management
 //ThemeContext-for implementing dark/light mode toggle across the app
+//Context Provider-stores and manipulates context data
+//Context Consumer-uses context data in components
 const ThemeContext = createContext({
   theme: "light",
 });
+
+const ThemeProvider = ({children}) =>{
+    const[theme, setTheme] = useState("light");
+    return(
+        <ThemeContext.Provider value={{ theme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+
+};
+export { ThemeContext, ThemeProvider };
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
-import { ToastContainer } from 'react-toastify'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "./context/themeContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
-    <ToastContainer/>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+      <ToastContainer />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
