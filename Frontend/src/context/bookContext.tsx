8322c4b56091309eda6ca750fd69fa1b061a65aa
ksamrat224 <ksamrat224@gmail.com>
2025-05-05 14:08:05 +0545

    bookcontext called in main.tsx

--- Frontend/src/context/bookContext.tsx
+++ Frontend/src/context/bookContext.tsx
@@ -1,6 +1,5 @@
 import React, {
   createContext,
-  use,
   useContext,
   useEffect,
   useMemo,
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -5,12 +5,15 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "./context/themeContext.tsx";
+import { BookProvider } from "./context/bookContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <BookProvider>
+          <App />
+        </BookProvider>
       </ThemeProvider>
       <ToastContainer />
     </BrowserRouter>
